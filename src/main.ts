6042e270381faeee765960d9866fdf7f39d43a83
Mika <mika.marcondes2@gmail.com>
2022-08-23 00:36:04 -0300

    Select a move

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
 import './style.css'
 
+const paper: any = document.getElementById('paper')
+const scissor: any = document.getElementById('scissor')
+const rock: any = document.getElementById('rock')
+
+paper.addEventListener("click", function () {selectMove(paper)})
+scissor.addEventListener("click", function () {selectMove(scissor)})
+rock.addEventListener("click", function () {selectMove(rock)})
+
+function selectMove(move: HTMLElement) {
+  console.log(move)
+}
+
