b03a602d1a23ac379b72a332fc081cf2d42d45e6
Mika <mika.marcondes2@gmail.com>
2022-08-23 11:43:12 -0300

    :sparkle: Added move selection

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,24 @@ paper?.addEventListener("click", function () {selectMove(1)})
 scissor?.addEventListener("click", function () {selectMove(2)})
 rock?.addEventListener("click", function () {selectMove(3)})
 
-function selectMove(playerMove: number) {
+function selectMove(this: any, playerMove: number) {
+  switch (playerMove) {
+    case 1:
+      paper?.removeEventListener("click", this)
+      scissor?.remove()
+      rock?.remove()
+      return
+    case 2:
+      scissor?.removeEventListener("click", this)
+      paper?.remove()
+      rock?.remove()
+      return
+    case 3:
+      rock?.removeEventListener("click", this)
+      scissor?.remove()
+      paper?.remove()
+      return
+  }
   console.log(playerMove)
 }
 
