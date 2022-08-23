2c3753dcfa7e60f8575b7aa7749c5f7342d9b72d
Mika <mika.marcondes2@gmail.com>
2022-08-23 11:44:36 -0300

    :recycle: Code refactor

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ const paper: HTMLElement | null = document.getElementById('paper')
 const scissor: HTMLElement | null = document.getElementById('scissor')
 const rock: HTMLElement | null = document.getElementById('rock')
 
-paper?.addEventListener("click", function () {selectMove(1)})
-scissor?.addEventListener("click", function () {selectMove(2)})
-rock?.addEventListener("click", function () {selectMove(3)})
+paper?.addEventListener("click", function () {
+  selectMove(1)
+})
+scissor?.addEventListener("click", function () {
+  selectMove(2)
+})
+rock?.addEventListener("click", function () {
+  selectMove(3)
+})
 
 function selectMove(this: any, playerMove: number) {
   switch (playerMove) {
