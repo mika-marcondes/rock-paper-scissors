2d067ab2fd6247089b18c65ca7c17b0f96be38cd
Mika <mika.marcondes2@gmail.com>
2022-08-23 17:57:20 -0300

    :recycle: Clean up

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import './style.css'
 
+const availableMoves = Array(0, 1, 2)
+
 const paper = document.getElementById('paper') as HTMLButtonElement
 const scissor = document.getElementById('scissor') as HTMLButtonElement
 const rock = document.getElementById('rock') as HTMLButtonElement
 
-paper.onclick = () => selectMove(0)
-scissor.onclick = () => selectMove(1)
-rock.onclick = () => selectMove(2)
+paper.onclick = () => playerMove(0)
+scissor.onclick = () => playerMove(1)
+rock.onclick = () => playerMove(2)
 
-const selectMove = (playerMove: number) => {
-  const hideUnselectedMoves = (userMove: HTMLButtonElement,
-                        remove1: HTMLButtonElement,
-                        remove2: HTMLButtonElement,
+const playerMove = (move: number) => {
+  const createPlayerPick = (userMove: HTMLButtonElement,
+                            remove1: HTMLButtonElement,
+                            remove2: HTMLButtonElement,
   ) => {
     userMove.onclick = null
     remove1.remove()
@@ -19,24 +21,21 @@ const selectMove = (playerMove: number) => {
     houseMove()
   };
   
-  switch (playerMove) {
+  switch (move) {
     case 0:
-      hideUnselectedMoves(paper, rock, scissor)
+      createPlayerPick(paper, rock, scissor)
       return
     case 1:
-      hideUnselectedMoves(scissor, paper, rock)
+      createPlayerPick(scissor, paper, rock)
       return
     case 2:
-      hideUnselectedMoves(rock, paper, scissor)
+      createPlayerPick(rock, paper, scissor)
       return
   }
 };
 
 const houseMove = () => {
-  let availableMoves: Array<number>
-  
-  availableMoves = Array(0, 1, 2)
-  let move = availableMoves[availableMoves.length * Math.random() | 0]
+  const move = availableMoves[availableMoves.length * Math.random() | 0]
   
   const createHousePick = () => {
     const button = document.createElement('button')
@@ -45,7 +44,7 @@ const houseMove = () => {
     
     gameArea?.appendChild(button)
   }
-
+  
   console.log(move)
   createHousePick()
 };
