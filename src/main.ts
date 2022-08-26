0f28f71a0547488e21b17ba688cc896d49238ed8
Mika <mika.marcondes2@gmail.com>
2022-08-26 19:04:33 -0300

    :recycle: Refactor code

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,6 @@ scissor.onclick = () => selectMove(1)
 rock.onclick = () => selectMove(2)
 
 const selectMove = (move: number) => {
-  const createPickScreen = (userMove: HTMLButtonElement,
-                            hideMove1: HTMLButtonElement,
-                            hideMove2: HTMLButtonElement,
-  ) => {
-    userMove.onclick = null
-    hideMove1.remove()
-    hideMove2.remove()
-    houseMove()
-  };
-  
   switch (move) {
     case 0:
       createPickScreen(paper, rock, scissor)
@@ -32,6 +22,16 @@ const selectMove = (move: number) => {
   }
 };
 
+const createPickScreen = (userMove: HTMLButtonElement,
+                          hideMove1: HTMLButtonElement,
+                          hideMove2: HTMLButtonElement,
+) => {
+  userMove.onclick = null
+  hideMove1.remove()
+  hideMove2.remove()
+  houseMove()
+};
+
 const availableMoves = Array('Paper', 'Scissor', 'Rock')
 
 const houseMove = () => {
@@ -41,7 +41,6 @@ const houseMove = () => {
     const button = document.createElement('button')
     const gameArea: HTMLElement | null = document.getElementById('game-area')
     button.innerHTML = `${move}`
-    
     gameArea?.appendChild(button)
   }
   
