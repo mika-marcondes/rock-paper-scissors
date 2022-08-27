1a9b8d97b5aad7d79e477fc8bc45363252333df8
Mika <mika.marcondes2@gmail.com>
2022-08-26 23:11:12 -0300

    :construction: wip: Play again button

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import './style.css'
 
 const paper = document.getElementById('paper') as HTMLButtonElement
-const scissor = document.getElementById('scissor') as HTMLButtonElement
+const scissors = document.getElementById('scissors') as HTMLButtonElement
 const rock = document.getElementById('rock') as HTMLButtonElement
 const gameArea: HTMLElement | null = document.querySelector('#game-area')
 
 paper.onclick = () => selectMove(0)
-scissor.onclick = () => selectMove(1)
+scissors.onclick = () => selectMove(1)
 rock.onclick = () => selectMove(2)
 
 const selectMove = (move: number) => {
   switch (move) {
     case 0:
-      createPickScreen(paper, rock, scissor)
+      createPickScreen(paper, rock, scissors)
       return
     case 1:
-      createPickScreen(scissor, paper, rock)
+      createPickScreen(scissors, paper, rock)
       return
     case 2:
-      createPickScreen(rock, paper, scissor)
+      createPickScreen(rock, paper, scissors)
       return
   }
 };
@@ -31,21 +31,25 @@ const createPickScreen = (userMove: HTMLButtonElement,
   hideMove1.remove()
   hideMove2.remove()
   houseMove()
+  playAgain()
 };
 
 const availableMoves = Array('paper', 'scissors', 'rock')
 
 const houseMove = () => {
-  const move = availableMoves[availableMoves.length * Math.random() | 0]
+  const housePick = availableMoves[availableMoves.length * Math.random() | 0]
   const button = document.createElement('button')
   
   gameArea?.appendChild(button)
   
   const createHouseMove = () => {
-    let src = `/images/icon-${move}.svg`
-    button.innerHTML += `<img src="${src}" alt="${move}" width="50" height="60">`
+    const src = `/images/icon-${housePick}.svg`
+    button.innerHTML += `<img src="${src}" alt="${housePick}" width="50" height="60">`
   }
   
-  console.log(move)
   setTimeout(createHouseMove, 1000)
 };
+
+const playAgain = () => {
+  document.querySelector('body')!.innerHTML += `<button>PLAY AGAIN</button>`
+}
