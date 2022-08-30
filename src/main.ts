afab050b01ec504544b48fc8ae4a05da7f4548c7
Mika <mika.marcondes2@gmail.com>
2022-08-30 01:35:42 -0300

    :recycle: Refactor code

--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,91 @@
-import './style.css'
+import "./style.css";
 
 interface AvailableMoves {
-  paper: HTMLButtonElement
-  scissors: HTMLButtonElement
-  rock: HTMLButtonElement
+  paper: HTMLButtonElement;
+  scissors: HTMLButtonElement;
+  rock: HTMLButtonElement;
 }
 
-let moves: AvailableMoves = <AvailableMoves> {
-  paper: document.getElementById('paper'),
-  scissors: document.getElementById('scissors'),
-  rock: document.getElementById('rock')
-}
+let moves: AvailableMoves = <AvailableMoves>{
+  paper: document.getElementById("paper"),
+  scissors: document.getElementById("scissors"),
+  rock: document.getElementById("rock"),
+};
 
-const gameArea: HTMLElement | null = document.querySelector('#game-area')
+const gameArea: HTMLElement | null = document.querySelector("#game-area");
 
 const selectMove = (select: number, moves: AvailableMoves) => {
   switch (select) {
     case 0:
-      showSelection(moves.paper, moves.rock, moves.scissors)
-      return
+      showSelection(moves.paper, moves.rock, moves.scissors);
+      return;
     case 1:
-      showSelection(moves.scissors, moves.paper, moves.rock)
-      return
+      showSelection(moves.scissors, moves.paper, moves.rock);
+      return;
     case 2:
-      showSelection(moves.rock, moves.paper, moves.scissors)
-      return
+      showSelection(moves.rock, moves.paper, moves.scissors);
+      return;
   }
 };
 
-const showSelection = (showSelected: HTMLButtonElement,
-                          hideUnselected1: HTMLButtonElement,
-                          hideUnselected2: HTMLButtonElement,
+const showSelection = (
+  showSelected: HTMLButtonElement,
+  hideUnselected1: HTMLButtonElement,
+  hideUnselected2: HTMLButtonElement
 ) => {
-  showSelected.onclick = null
-  showSelected.style.display = ""
-  hideUnselected1.style.display = "none"
-  hideUnselected2.style.display = "none"
-  displayHouseMove()
-  setTimeout(playAgain, 1500)
+  showSelected.onclick = null;
+  showSelected.style.display = "";
+  hideUnselected1.style.display = "none";
+  hideUnselected2.style.display = "none";
+  displayHouseMove();
+  setTimeout(playAgain, 1500);
 };
 
 const displayHouseMove = () => {
-  const button = document.createElement('button')
+  const button = document.createElement("button");
 
-  gameArea?.appendChild(button)
-  
+  gameArea?.appendChild(button);
+
   const createHouseMove = () => {
-    const availableMoves = Array('paper', 'scissors', 'rock')
-    const housePick = availableMoves[availableMoves.length * Math.random() | 0]
+    const availableMoves = Array("paper", "scissors", "rock");
+    const housePick =
+      availableMoves[(availableMoves.length * Math.random()) | 0];
 
-    button.setAttribute("id", `${housePick}`)
-  }
-  
-  setTimeout(createHouseMove, 1000)
+    button.setAttribute("id", `${housePick}`);
+  };
+
+  setTimeout(createHouseMove, 1000);
 };
 
 const playAgain = () => {
-  const buttonPlayAgain = document.createElement("button")
-  const body = document.querySelector("body")
+  const buttonPlayAgain = document.createElement("button");
+  const body = document.querySelector("body");
 
-  buttonPlayAgain.innerHTML += "Play again"
-  body?.appendChild(buttonPlayAgain)
+  buttonPlayAgain.innerHTML += "Play again";
+  body?.appendChild(buttonPlayAgain);
 
-  buttonPlayAgain.onclick = () => resetDisplay(buttonPlayAgain, moves)
-}
+  buttonPlayAgain.onclick = () => resetDisplay(buttonPlayAgain, moves);
+};
 
-const resetDisplay = (buttonPlayAgain: HTMLButtonElement, moves: AvailableMoves) => {
-  const game = document.getElementById('game-area')
+const resetDisplay = (
+  buttonPlayAgain: HTMLButtonElement,
+  moves: AvailableMoves
+) => {
+  const game = document.getElementById("game-area");
 
-  moves.paper.style.display = ""
-  moves.rock.style.display = ""
-  moves.scissors.style.display = ""
+  moves.paper.style.display = "";
+  moves.rock.style.display = "";
+  moves.scissors.style.display = "";
 
-  moves.paper.onclick = () => selectMove(0, moves)
-  moves.scissors.onclick = () => selectMove(1, moves)
-  moves.rock.onclick = () => selectMove(2, moves)
+  moves.paper.onclick = () => selectMove(0, moves);
+  moves.scissors.onclick = () => selectMove(1, moves);
+  moves.rock.onclick = () => selectMove(2, moves);
 
-  if (game?.hasChildNodes())
-    game.removeChild(game.children[3])
-  
-  buttonPlayAgain.remove()
-}
+  if (game?.hasChildNodes()) game.removeChild(game.children[3]);
 
-moves.paper.onclick = () => selectMove(0, moves)
-moves.scissors.onclick = () => selectMove(1, moves)
-moves.rock.onclick = () => selectMove(2, moves)
+  buttonPlayAgain.remove();
+};
+
+moves.paper.onclick = () => selectMove(0, moves);
+moves.scissors.onclick = () => selectMove(1, moves);
+moves.rock.onclick = () => selectMove(2, moves);
